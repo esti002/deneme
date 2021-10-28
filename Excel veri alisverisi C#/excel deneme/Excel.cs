53c68d63be3cd670ed691e0f4a44d4afcc16e656
esti002 <85763881+esti002@users.noreply.github.com>
2021-10-29 01:02:35 +0300

    Add files via upload

---
+++ Excel veri alisverisi C#/excel deneme/Excel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+using _Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace excel_deneme
+{
+    class Excel
+    {
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+
+        public Excel(string path, int Sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = excel.Worksheets[Sheet];
+        }
+        public string ReadCell(int i, int j)
+        {
+            i++;
+            j++;
+            Object valor = ((_Range)ws.Cells[i, j]).Value2;
+            if (valor != null)
+            {
+                return System.Convert.ToString(valor); 
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public void WriteCell(int i, int j,string s)
+        {
+            i++;
+            j++;
+
+            ws.Cells[i, j] = s;
+            wb.Save();
+        }
+
+
+        public void closeWb()
+        {
+            wb.Close();
+        }
+    }
+}
---
+++ Excel veri alisverisi C#/excel deneme/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace excel_deneme
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFile();
+
+        }
+
+        public void openFile()
+        {
+            Excel excel = new Excel(@"C:\Users\Mehmet ESTİ\OneDrive\Kitap1.xlsx", 1);
+            MessageBox.Show(excel.ReadCell(0,0));
+            excel.closeWb();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Excel excel = new Excel(@"C:\Users\Mehmet ESTİ\OneDrive\Kitap1.xlsx", 1);
+            excel.WriteCell(5, 6, textBox1.Text);
+            MessageBox.Show("veriler guncellendi.");
+            excel.closeWb();
+            
+
+        }
+    }
+}
